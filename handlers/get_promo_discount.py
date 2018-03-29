bb0ee768e106762d825a5c3015ede0974aabf513
Tom McLaughlin <tom@serverlessops.io>
2018-03-28 21:18:56 -0400

    Cleanup IOpipe handling

--- handlers/get_promo_discount.py
+++ handlers/get_promo_discount.py
@@ -6,13 +6,13 @@ import os
 import random
 import time
 
-if os.environ.get('XRAY_ENABLED', '').lower() == 'true':
-    from aws_xray_sdk.core import xray_recorder
-    from aws_xray_sdk.core import patch_all
-
 from iopipe.iopipe import IOpipe
 from iopipe.contrib.profiler import ProfilerPlugin
 from iopipe.contrib.trace import TracePlugin
+
+if os.environ.get('XRAY_ENABLED', '').lower() == 'true':
+    from aws_xray_sdk.core import xray_recorder
+    from aws_xray_sdk.core import patch_all
 
 # logging
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
@@ -24,13 +24,20 @@ if os.environ.get('XRAY_ENABLED', '').lower() == 'true':
     patch_all()
 
 # IOpipe
+iopipe_plugins = []
+
 iopipe_profiler_enabled = os.environ.get('IOPIPE_PROFILER_ENABLED', '').lower() == 'true'
-iopipe_profiler_plugin = ProfilerPlugin(enabled=iopipe_profiler_enabled)
-iopipe_tracing_plugin = TracePlugin(auto_measure=True)
+if iopipe_profiler_enabled:
+    iopipe_profiler_plugin = ProfilerPlugin(enabled=True)
+    iopipe_plugins.append(iopipe_profiler_plugin)
+
+iopipe_tracing_enabled = os.environ.get('IOPIPE_TRACING_ENABLED', '').lower() == 'true'
+if iopipe_tracing_enabled:
+    iopipe_tracing_plugin = TracePlugin(auto_measure=True)
+    iopipe_plugins.append(iopipe_tracing_plugin)
 
 iopipe_token = os.environ.get('IOPIPE_TOKEN') or None
-iopipe = IOpipe(iopipe_token,
-                plugins=[iopipe_profiler_plugin, iopipe_tracing_plugin])
+iopipe = IOpipe(iopipe_token, plugins=iopipe_plugins)
 
 
 def handler(event, context):
